38ba67bfdaaa67b4f17bf8e790704e8bc344055e
Dwight Lidman <dwight.lidman@gmail.com>
2018-11-28 14:52:46 +0100

    gold std debugging

--- src/gold_std.py
+++ src/gold_std.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from keras.utils import to_categorical
+#from keras.utils import to_categorical
 from utils import trans_mut_map
 from docria.storage import DocumentIO
 from argparse import ArgumentParser
@@ -19,48 +19,46 @@ def to_categories(index, categories):
 def json_compatible(index):
     def make_dict(span, entity):
         return {'start': span[0], 'stop': span[1],
-                'entity': '-'.join(entity)}
-    for i in range(len(index)):
-        index[i] = [make_dict(s,e) for s,e in index[i].items()]
-    return index
+                'class': '-'.join(entity)}
+    return [make_dict(s,e) for s,e in index.items()]
 
-def gold_std_idx(doc_reader):
+def gold_std_idx(docria):
     labels, types = set(), set()
-    index = []
-    for doc in doc_reader:
-        doc_index = {}
-        longest = None
-        for node in doc.layers['tac/entity/gold']:
-            labels.add(node.fld.label)
-            types.add(node.fld.type)
-            entity = node.fld.text
-            # ignore xml-only entities
-            if entity:
-                span = (entity.start, entity.stop)
-                # TODO: simplify logic if possible
-                if longest:
-                    # there is a greater span with same start
-                    if span[0] == longest[0] and span[1] > longest[1]:
-                        del doc_index[longest]
-                        longest = span
-                    # we are either inside or after the current span
-                    if span[0] > longest[0]:
-                        # this should never happen (overlapping spans)
-                        if span[0] <= longest[1] and span[1] > longest[1]:
-                            print(entity, doc.texts['main'][longest[0]:longest[1]])
-                            continue
-                            raise ValueError("Span %s and Span %s are overlapping"
-                                            % (longest, span))
-                        # we are inside the span
-                        if span[1] <= longest[1]:
-                            continue
-                        # we have a new span
-                        else:
+    index = {}
+    with DocumentIO.read(docria) as doc_reader:
+        for doc in doc_reader:
+            doc_index = {}
+            longest = None
+            for node in doc.layers['tac/entity/gold']:
+                labels.add(node.fld.label)
+                types.add(node.fld.type)
+                entity = node.fld.text
+                # ignore xml-only entities
+                if entity:
+                    span = (entity.start, entity.stop)
+                    # TODO: simplify logic if possible
+                    if longest:
+                        # there is a greater span with same start
+                        if span[0] == longest[0] and span[1] > longest[1]:
+                            del doc_index[longest]
                             longest = span
-                else:
-                    longest = span
-                doc_index[span] = (node.fld.type, node.fld.label)
-        index.append(doc_index)
+                        # we are either inside or after the current span
+                        if span[0] > longest[0]:
+                            # this should never happen (overlapping spans)
+                            if span[0] <= longest[1] and span[1] > longest[1]:
+                                continue
+                                raise ValueError("Span %s and Span %s are overlapping"
+                                                % (longest, span))
+                            # we are inside the span
+                            if span[1] <= longest[1]:
+                                continue
+                            # we have a new span
+                            else:
+                                longest = span
+                    else:
+                        longest = span
+                    doc_index[span] = (node.fld.type, node.fld.label)
+            index[doc.props['docid']] = doc_index
     # TODO: ensure ordering is consistent between function calls
     categories = {pair: index for index, pair in enumerate(product(types, labels))}
     return index, categories
@@ -71,12 +69,19 @@ def get_args():
     return parser.parse_args()
 
 
+def get_doc_index(docria, gold_std):
+    index = {}
+    with DocumentIO.read(docria) as reader:
+        for doc in reader:
+            docid = doc.props['docid']
+            index[docid] = {'text': str(doc.texts['main']),
+                            'gold': json_compatible(gold_std[docid])}
+    return index
+
+
 if __name__ == '__main__':
     args = get_args()
     if args.file:
-        with DocumentIO.read(args.file) as docria:
-            index, cats = gold_std_idx(docria)
-            for doc in json_compatible(index):
-                for entity in doc:
-                    print(entity)
-
+        gold_std, _ = gold_std_idx(args.file)
+        index = get_doc_index(args.file, gold_std)
+        print(index)
--- src/server.py
+++ src/server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 from utils import langforia
+from pathlib import Path
+from gold_std import get_doc_index, gold_std_idx
 
 app = Flask(__name__)
 
@@ -13,3 +15,11 @@ def entity_linking():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/gold')
+def doc_index():
+    path = Path('../corpus/tac/en/eng.2015.eval.docria')
+    if path.exists():
+        gold_std, _ = gold_std_idx(path)
+        index = get_doc_index(path, gold_std)
+        return index
