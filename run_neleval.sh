d70fe63aac42c7481b3bd3cdf3b223884640b642
Johannes Ahnlide <johannes@voxel.se>
2019-01-03 16:33:59 +0100

    Neleval script better comments and warnings

--- run_neleval.sh
+++ run_neleval.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-: >systems/predict.all.tsv
-for f in predict.*.tsv; do
+REPLY='y'
+
+if ! git diff-index --quiet HEAD --; then
+        git diff --compact-summary HEAD
+        echo
+        read -p "The index is not clean. Do you want to continue? (y/n) " -n 1 -r
+        echo
+fi
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+
+        : >systems/predict.all.tsv
+        for f in predict.*.tsv; do
+                (
+                        cat "$f"
+                        echo
+                ) >>systems/predict.all.tsv
+        done
+        cd neleval
+        ./scripts/run_tac16_evaluation.sh ../corpus/tac/gold/tac_kbp_2017_edl_evaluation_gold_standard_entity_mentions.tab ../systems ../out 1
+        cd ..
+        echo "Please describe the current run"
+        read comment
+
         (
-                cat "$f"
-                echo
-        ) >>systems/predict.all.tsv
-done
-cd neleval
-./scripts/run_tac16_evaluation.sh ../corpus/tac/gold/tac_kbp_2017_edl_evaluation_gold_standard_entity_mentions.tab ../systems ../out 1
-cd ..
-echo "Please describe the current run"
-read comment
+                echo "# $(git log -1 --pretty="format:%h %s %d")"
 
-(
-        echo "# $(git log -1 --pretty=oneline)"
-        echo "# $comment" >>out/predict.log
-        cat out/predict.all.tsv.evaluation
-) >>out/predict.log
+                echo "# $comment" >>out/predict.log
+                cat out/predict.all.tsv.evaluation
+        ) >>out/predict.log
 
-less +G out/predict.log
+        less +G out/predict.log
+fi
